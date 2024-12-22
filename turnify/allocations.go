b8d42bedc454ed162e59dcf2bb2a37bf75d441f8
Nicola Gallo <nicolagmt@hotmail.com>
2024-12-22 20:13:52 +0100

    working on allocation

--- turnify/allocations.go
+++ turnify/allocations.go
@@ -2,13 +2,54 @@
 
 package turnify
 
-func AllocateWorkers(workers []*Worker, shifts []WorkShift) {
+import "fmt"
+
+// WorkerIsAvailable checks if a worker is available.
+func WorkerIsAvailable(worker *Worker, shift WorkShift) bool {
+	for _, workShift := range worker.WorkShifts {
+		// Check if the worker already has a shift on the same day
+		if workShift.Date.Equal(shift.Date) {
+			return false
+		}
+
+		// Calculate the previous and next days
+		prevDay := shift.Date.AddDate(0, 0, -1)
+		nextDay := shift.Date.AddDate(0, 0, 1)
+
+		// Check if the worker has a night shift on the previous day and the requested shift is a day shift
+		if workShift.Date.Equal(prevDay) && workShift.NightShift && !shift.NightShift {
+			return false
+		}
+
+		// Check if the worker has a day shift on the next day and the requested shift is a night shift
+		if workShift.Date.Equal(nextDay) && !workShift.NightShift && shift.NightShift {
+			return false
+		}
+	}
+
+	// If none of the conditions are met, the worker is available
+	return true
+}
+
+
+// AllocateWorkers allocates workers to shifts.
+func AllocateWorkers(workers []*Worker, shifts []WorkShift) error {
+	notAllocated := []WorkShift{}
 	for _, shift := range shifts {
-		for _, worker := range workers {
-			if worker.WorkShifts == nil {
-				worker.WorkShifts = []WorkShift{}
+		randomNumber := GenerateRandomNumbers(len(workers))
+		for _, i := range randomNumber {
+			worker := workers[i]
+			if WorkerIsAvailable(worker, shift) {
+				worker.WorkShifts = append(worker.WorkShifts, shift)
+				break
 			}
-			worker.WorkShifts = append(worker.WorkShifts, shift)
+			if i == len(workers) - 1 {
+				notAllocated = append(notAllocated, shift)
+			}
 		}
 	}	
+	if len(notAllocated) > 0 {
+		return fmt.Errorf("Not all shifts were allocated")
+	}
+	return nil
 }
